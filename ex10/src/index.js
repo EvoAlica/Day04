26a8e91f70d33eeec1b522d936e95ab10b12c954
Anela Alic <ane@Tariks-MacBook-Air.local>
2021-03-12 01:37:27 +0100

    DONE

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main(myFirstName, myLastName, myHomeTown){
+    // only change code below this line
+    var myFirstName = "Tarik";
+    var myLastName = "Alic";
+    var myHomeTown = "Gorazde";
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+console.log(main());
+module.exports = main;
