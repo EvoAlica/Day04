6fc6c30887688422c1be113239289223e8cd245e
Anela Alic <ane@Tariks-MacBook-Air.local>
2021-03-12 01:36:41 +0100

    Added ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,18 @@
+function main(firstNum, secondNum, thirdNum){
+    // only change code below this line
+    var firstNum = 6;
+    var secondNum = 25;
+    var thirdNum = 5.2;
+        firstNum *= 9;
+        secondNum /= 5;
+        thirdNum *= 10;
+    // Only change code above this line
+    return {
+        firstNum,
+        secondNum,
+        thirdNum
+    };
+}
+
+console.log(main());
+module.exports = main;
